dbf9793934ae05f5d7429b2e59c9edcbbe0048da
ayaaeid <ayaaeid.72@gmail.com>
2023-03-26 16:37:30 +0200

    add drwer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,6 +7,54 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/img/test.jpg"),
+                        fit: BoxFit.cover),
+                  ),
+                  accountName: Text("ali Hassan",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      )),
+                  accountEmail: Text("ali@yahoo.com"),
+                  currentAccountPictureSize: Size.square(99),
+                  currentAccountPicture: CircleAvatar(
+                      radius: 55,
+                      backgroundImage: AssetImage("assets/img/ali.jpg")),
+                ),
+                ListTile(
+                    title: Text("Home"),
+                    leading: Icon(Icons.home),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("My products"),
+                    leading: Icon(Icons.add_shopping_cart),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("About"),
+                    leading: Icon(Icons.help_center),
+                    onTap: () {}),
+                ListTile(
+                    title: Text("Logout"),
+                    leading: Icon(Icons.exit_to_app),
+                    onTap: () {}),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 12),
+              child: Text("Developed by Ali Hassan © 2022",
+                  style: TextStyle(fontSize: 16)),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 76, 141, 95),
         title: Text("Home"),
