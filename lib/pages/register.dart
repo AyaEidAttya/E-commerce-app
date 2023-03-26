a6232b23ec0f5859adcdfea4ddbdee134913f109
ayaaeid <ayaaeid.72@gmail.com>
2023-03-26 15:44:15 +0200

    add SingleChildScrollView

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -17,72 +17,74 @@ class Register extends StatelessWidget {
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(33.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 64,
-                ),
-                // ignore: prefer_const_constructors
-                TextField(
-                  keyboardType: TextInputType.text,
-                  obscureText: false,
-                  decoration: decorationtextfield.copyWith(
-                      hintText: "enter your user name"),
-                ),
-
-                const SizedBox(
-                  height: 33,
-                ),
-
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: decorationtextfield.copyWith(
-                      hintText: "enter your email"),
-                ),
-                const SizedBox(
-                  height: 33,
-                ),
-                TextField(
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  decoration: decorationtextfield.copyWith(
-                      hintText: "enter your password"),
-                ),
-
-                const SizedBox(
-                  height: 33,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(BTNgreen),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8))),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 64,
                   ),
-                  child: Text(
-                    "Siqn in",
-                    style: TextStyle(fontSize: 19),
+                  // ignore: prefer_const_constructors
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: false,
+                    decoration: decorationtextfield.copyWith(
+                        hintText: "enter your user name"),
                   ),
-                ),
-                const SizedBox(
-                  height: 33,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("dont have an acount?",
-                        style: TextStyle(fontSize: 20)),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text('sign upt',
-                          style: TextStyle(color: Colors.black, fontSize: 20)),
-                    )
-                  ],
-                )
-              ],
+            
+                  const SizedBox(
+                    height: 33,
+                  ),
+            
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: decorationtextfield.copyWith(
+                        hintText: "enter your email"),
+                  ),
+                  const SizedBox(
+                    height: 33,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decorationtextfield.copyWith(
+                        hintText: "enter your password"),
+                  ),
+            
+                  const SizedBox(
+                    height: 33,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(BTNgreen),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                    ),
+                    child: Text(
+                      "Siqn in",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 33,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("dont have an acount?",
+                          style: TextStyle(fontSize: 20)),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('sign upt',
+                            style: TextStyle(color: Colors.black, fontSize: 20)),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
