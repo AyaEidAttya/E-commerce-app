d21977ea1eaa28d935988945537c2fb75b0c0d7f
ayaaeid <ayaaeid.72@gmail.com>
2023-03-30 13:45:51 +0200

    import to item file

---
+++ lib/Model/iteams_class.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+class iteam{
+  String imgpath;
+  double price;
+  iteam({
+    required this.imgpath,required this.price
+  });
+}
+
+final List<iteam> items=[
+iteam(
+  imgpath: "assets/img/OIP (1).jpeg",price: 12.99),
+  iteam(
+  imgpath: "assets/img/OIP (2).jpeg",price: 12.99),
+  iteam(
+  imgpath: "assets/img/OIP (3).jpeg",price: 12.99),
+  iteam(
+  imgpath: "assets/img/450951-flowers-nature-plant-beautiful-green-flower (1).jpg",price: 12.99),
+  iteam(
+  imgpath: "assets/img/450960-flowers-nature-plant-beautiful-green-flower (1).jpg",price: 12.99),
+   iteam(
+  imgpath: "assets/img/3832504d6d10b7ea7958ff9e25bd14c3.jpg",price: 12.99),
+   iteam(
+  imgpath: "assets/img/Green_Flowers.jpg",price: 12.99),
+iteam(
+  imgpath: "assets/img/R.jpeg",price: 12.99),
+];
